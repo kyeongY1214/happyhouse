81a5f416ccefcba4a527492c3ca86f27bdbb048e
KyeongY1214 <ykh1214@naver.com>
2021-05-25 16:28:57 +0900

    다시

--- happyfinal/src/main/java/com/ssafy/model/util/PageNavigation.java
+++ happyfinal/src/main/java/com/ssafy/model/util/PageNavigation.java
@@ -82,7 +82,7 @@ public class PageNavigation {
 
 	public void makeNavigator() {
 		int startPage = (currentPage - 1) / naviSize * naviSize + 1;
-		int endPage = startPage + naviSize - 1;
+		int endPage = (startPage + naviSize - 1) /2;
 		if(totalPageCount < endPage)
 			endPage = totalPageCount;
 		
